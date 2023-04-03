23cd0c0e74d60b07da957189d885f428108fb216
James <james@Zipcoders-MacBook-Pro-101.local>
2023-04-03 09:03:27 -0400

    finished commenting questions

--- answers/part2.sql
+++ answers/part2.sql
@@ -1,15 +1,15 @@
 Part 2:
-//What are all the types of pokemon that a pokemon can have?
+-- //What are all the types of pokemon that a pokemon can have?
 SELECT * FROM types;
 
-//What is the name of the pokemon with id 45?
+--//What is the name of the pokemon with id 45?
 SELECT * FROM pokemons WHERE id = 45;
 
-//How many pokemon are there?
+-- //How many pokemon are there?
 SELECT COUNT(id) FROM pokemons;
 
-//How many types are there?
+-- //How many types are there?
 SELECT COUNT(id) FROM types;
 
-//How many pokemon have a secondary type?
+-- //How many pokemon have a secondary type?
 SELECT COUNT(id) FROM pokemons WHERE secondary_type IS NOT NULL;
--- answers/part4.sql
+++ answers/part4.sql
@@ -1,4 +1,6 @@
 Part 4:
+-- Directions: Write a query that returns the following collumns:
+
 SELECT pokemons.name, trainers.trainername, pokemon_trainer.pokelevel,
 a.name AS primary_type, b.name AS secondary_type
 FROM pokemons
@@ -7,5 +9,6 @@ INNER JOIN types AS b ON secondary_type = b.id
 INNER JOIN pokemon_trainer ON pokemon_trainer.pokemon_id = pokemons.id
 INNER JOIN trainers ON trainers.trainerID = pokemon_trainer.trainerID
 ORDER BY pokemon_trainer.pokelevel DESC
+
 
 
