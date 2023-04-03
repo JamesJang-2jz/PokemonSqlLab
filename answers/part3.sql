1f9dea6485d918fa098f4b371a1400236011e1b4
James <james@Zipcoders-MacBook-Pro-101.local>
2023-04-02 23:00:41 -0400

    finished part 4

--- answers/part3.sql
+++ answers/part3.sql
@@ -1,59 +1,43 @@
-Part 2:
-What are all the types of pokemon that a pokemon can have?
-SELECT * FROM types;
-
-What is the name of the pokemon with id 45?
-SELECT * FROM pokemons WHERE id = 45;
-
-How many pokemon are there?
-SELECT COUNT(id) FROM pokemons;
-
-How many types are there?
-SELECT COUNT(id) FROM types;
-
-How many pokemon have a secondary type?
-SELECT COUNT(id) FROM pokemons WHERE secondary_type IS NOT NULL;
-
 Part 3:
-What is each pokemon's primary type?
+//What is each pokemon's primary type?
 SELECT pokemons.name, types.name
 FROM pokemons
 LEFT JOIN types
 ON pokemons.primary_type = types.id;
 
-What is Rufflet's secondary type?
+//What is Rufflet's secondary type?
 SELECT pokemons.name, types.name
     -> FROM pokemons
     -> INNER JOIN types
     -> ON pokemons.secondary_type = types.id
     -> WHERE pokemons.name = 'Rufflet';
 
-What are the names of the pokemon that belong to the trainer with trainerID 303?
+// What are the names of the pokemon that belong to the trainer with trainerID 303?
 SELECT trainerID, pokemons.name
 FROM pokemon_trainer
 LEFT JOIN pokemons
 ON pokemon_trainer.pokemon_id = pokemons.id
 WHERE pokemon_trainer.trainerID = 303;
 
-//SELECT count(secondary_type)
+//SELECT count(secondary_type) // if you know the number for poison type
 FROM pokemons
 WHERE secondary_type = 7;
 
-How many pokemon have a secondary type Poison
+//How many pokemon have a secondary type Poison
 select COUNT(secondary_type)
     -> FROM pokemons
     -> INNER JOIN types
     -> ON pokemons.secondary_type = types.id
     -> WHERE types.name = 'Poison';
 
-What are all the primary types and how many pokemon have that type?
+//What are all the primary types and how many pokemon have that type?
 SELECT types.name, COUNT(pokemons.id) AS NumberPokemonByTypes
     -> FROM pokemons
     -> LEFT JOIN types
     -> ON pokemons.primary_type = types.id
     -> GROUP BY types.name;
 
-How many pokemon at level 100 does each trainer with at least one level 100 pokemone have? (Hint: your query should not display a trainer
+//How many pokemon at level 100 does each trainer with at least one level 100 pokemone have? (Hint: your query should not display a trainer
 SELECT trainerID, COUNT(pokemon_id) as Level100Pokemon
     -> FROM pokemon_trainer
     -> WHERE pokelevel = 100
